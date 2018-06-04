25c579ebb6316a3b8a4b7efe08febe009db051e9
skybearlove <34184491+skybearlove@users.noreply.github.com>
2018-06-04 15:23:47 +0800

    Create SendEmail.py

---
+++ SendEmail.py
@@ -0,0 +1,53 @@
+import socket  
+import time  
+import urllib
+import smtplib  
+from email.mime.multipart import MIMEMultipart  
+from email.mime.text import MIMEText    
+    
+def sendEmail(smtpserver,username,password,sender,receiver,subject,msghtml):  
+    msg = MIMEMultipart()  
+    msg["To"] = ','.join(receiver) 
+    msg["From"] = sender  
+    msg['Subject'] =  subject  
+    msgText = MIMEText(msghtml,'html','utf-8')  
+    msg.attach(msgText)  
+    #sendEmail  
+    smtp = smtplib.SMTP()  
+    smtp.connect(smtpserver)  
+    smtp.login(username, password)  
+    smtp.sendmail(sender, receiver, msg.as_string())  
+    smtp.quit() 
+
+def get_ip_address():  
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
+    s.connect(("1.1.1.1",80))  
+    ipAddr=s.getsockname()[0]  
+    s.close()  
+    return ipAddr 
+
+def check_network():  
+    while True:  
+        try:  
+            result=urllib.request.urlopen('http://www.google.com').read()  
+            print("Network is Ready!")  
+            break  
+        except Exception:  
+            print("Network is not ready,Sleep 5s....")
+            time.sleep(5)  
+    return True
+
+def get_ip_address():  
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
+    s.connect(("1.1.1.1",80))  
+    ipAddr=s.getsockname()[0]  
+    s.close()  
+    return ipAddr  
+
+
+
+if __name__ == '__main__':  
+    check_network()  
+    ipAddr=get_ip_address()  
+    sendEmail('smtp.cc.ncu.edu.tw','xxxxxx','xxxx','xxxxxx@cc.ncu.edu.tw',['xxxxxx@gmail.com','xxxxxx@oz.nthu.edu.tw'],'IP Address Of Raspberry Pi3',ipAddr) 
+    print('Email Sended')
